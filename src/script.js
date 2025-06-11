25ae40f8b5905ff8ef944664245d8ba6eec9a4b8
Neel Bhavsar <neelbhavsarr1@gmail.com>
2025-06-12 00:56:47 +0100

    Initial commit of portfolio website

---
+++ src/script.js
@@ -0,0 +1,27 @@
+//js script for image enlargment in achievements scetion
+
+document.addEventListener('DOMContentLoaded', () => {
+  const images = document.querySelectorAll('.zoomin');
+
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      const modal = document.createElement('div');
+      modal.style.position = 'fixed';
+      modal.style.top = 0;
+      modal.style.left = 0;
+      modal.style.width = '100%';
+      modal.style.height = '100%';
+      modal.style.backgroundColor = 'rgba(0,0,0,0.8)';
+      modal.style.display = 'flex';
+      modal.style.justifyContent = 'center';
+      modal.style.alignItems = 'center';
+      modal.style.zIndex = '9999';
+      modal.innerHTML = `<img src="${image.src}" style="max-width: 90%; max-height: 90%; border-radius: 10px;" />`;
+
+
+      modal.addEventListener('click', () => document.body.removeChild(modal));
+
+      document.body.appendChild(modal);
+    });
+  });
+});
